d02cfb020b7e2ea03c07d543b7bf483d17f97821
Ayush <ayush15may@gmail.com>
2018-11-09 22:35:28 +0530

    feat: adding mobile first styling media query

---
+++ src/components/head.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Helmet from 'react-helmet';
+
+export default function Head() {
+  return (
+    <Helmet>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+    </Helmet>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,21 @@
 import * as React from 'react';
-import Helmet from 'react-helmet';
 import 'normalize.css';
+import '../styles/main.css';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import SalmonCeleryHoney from '../theme/salmon-celery-honey';
 import LayoutContainer from '../components/layout-container';
 import LeftHalf from '../components/left-half';
 import RightHalf from '../components/right-half';
 import SimpleAppBar from '../components/simple-app-bar';
-
-const styles = {
-  minHeight: '100vh',
-  display: 'grid',
-  gridTemplateRows: 'auto 1fr'
-};
+import Head from '../components/head';
 
 export default class Main extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Helmet>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-        </Helmet>
+        <Head />
         <MuiThemeProvider theme={SalmonCeleryHoney}>
-          <div style={styles}>
+          <div className="app-root">
             <SimpleAppBar />
             <LayoutContainer children={[LeftHalf, RightHalf]} />
           </div>
