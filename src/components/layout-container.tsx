76054b0df64606f0d58f26362aa07d37943228a7
Ayush <ayush15may@gmail.com>
2018-11-09 22:35:28 +0530

    feat: adding mobile first styling media query

--- src/components/layout-container.tsx
+++ src/components/layout-container.tsx
@@ -1,27 +1,18 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import HalfBox from './half-box';
 
-const styles = {
-  root: {
-    display: 'grid',
-    gridTemplateColumns: '1fr 1fr',
-    maxHeight: '100%'
-  }
-};
-
-function LayoutContainer(props) {
-  const { classes, children } = props;
+const LayoutContainer = props => {
+  const { children } = props;
   const childElements = children.map((child, index) => {
     return <HalfBox key={index.toString()}>{child()}</HalfBox>;
   });
 
-  return <div className={classes.root}>{childElements}</div>;
-}
-
-LayoutContainer.propTypes = {
-  classes: PropTypes.object.isRequired
+  return <div className="layout-root">{childElements}</div>;
 };
 
-export default withStyles(styles)(LayoutContainer);
+LayoutContainer.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default LayoutContainer;
