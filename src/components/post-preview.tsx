3df512ca01edc14e5f5661a603370c67d77f839d
Ayush <ayush15may@gmail.com>
2018-11-27 18:47:16 +0530

    added links to redirect to corresponding pages

--- src/components/post-preview.tsx
+++ src/components/post-preview.tsx
@@ -4,6 +4,7 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'gatsby';
 
 const cardStyle = {
   marginBottom: '1rem'
@@ -13,15 +14,17 @@ const PostPreview = props => {
   const { postInfo } = props;
   return (
     <Card style={cardStyle}>
-      <CardActionArea>
-        <CardMedia component="img" alt={postInfo.imageTitle} image={postInfo.imageURL} title={postInfo.imageTitle} />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {postInfo.title}
-          </Typography>
-          <Typography component="p">{postInfo.subtitle}</Typography>
-        </CardContent>
-      </CardActionArea>
+      <Link to={postInfo.slug} style={{ textDecoration: 'none' }}>
+        <CardActionArea>
+          <CardMedia component="img" alt={postInfo.imageTitle} image={postInfo.imageURL} title={postInfo.imageTitle} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {postInfo.title}
+            </Typography>
+            <Typography component="p">{postInfo.subtitle}</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
     </Card>
   );
 };
