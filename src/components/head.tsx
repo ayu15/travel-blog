2be66f702a016d382033a7726f0e0d4654fc90aa
Ayush <ayush15may@gmail.com>
2018-12-23 11:53:11 +0530

    try-3

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -6,6 +6,21 @@ export default function Head() {
     <Helmet>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
       <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          if (window.netlifyIdentity) {
+            window.netlifyIdentity.on("init", user => {
+              if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                  document.location.href = "/admin/";
+                });
+              }
+            });
+          }
+        `
+        }}
+      />
     </Helmet>
   );
 }
