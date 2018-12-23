05a7e3c776aa8c8746fff1a7be7d735716e3c70a
Ayush <ayush15may@gmail.com>
2018-12-23 10:23:45 +0530

    common identity

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -5,6 +5,7 @@ export default function Head() {
   return (
     <Helmet>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+      <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
     </Helmet>
   );
 }
