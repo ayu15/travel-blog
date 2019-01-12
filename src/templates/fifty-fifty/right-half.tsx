54f25f2e996e674effb9202205980610cf90c5a1
Ayush <ayush15may@gmail.com>
2019-01-12 19:33:32 +0530

    adding updated at date

--- src/templates/fifty-fifty/right-half.tsx
+++ src/templates/fifty-fifty/right-half.tsx
@@ -3,10 +3,14 @@ import DisqusEmbed from '../../components/disqus-embed';
 
 const RightHalf = ({ data }) => {
   const { frontmatter, html, fields } = data;
+  console.log('frontmatter is', frontmatter);
 
   return (
     <div className="fifty-fifty-root-right-half">
-      <h1>{frontmatter.title}</h1>
+      <h1 className="post-title">{frontmatter.title}</h1>
+      <p className="post-date">
+        <em>updated at - {frontmatter.date}</em>
+      </p>
       <p>{frontmatter.subtitle}</p>
       <div dangerouslySetInnerHTML={{ __html: html }} />
       <DisqusEmbed config={{ slug: fields.slug, title: frontmatter.title }} />
--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -2,8 +2,8 @@ import Typography from 'typography';
 import usWebDesignStandardsTheme from 'typography-theme-us-web-design-standards';
 
 usWebDesignStandardsTheme.baseFontSize = '16px';
-usWebDesignStandardsTheme.baseLineHeight = 1.48;
-usWebDesignStandardsTheme.scale = 2;
+usWebDesignStandardsTheme.baseLineHeight = 1.25;
+usWebDesignStandardsTheme.scale = 1.6;
 usWebDesignStandardsTheme.rhythm = 1;
 
 const typography = new Typography(usWebDesignStandardsTheme);
