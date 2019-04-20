51e0ad8d14831a882d7b3d74bf1753b71bd00460
Ayush <ayush15may@gmail.com>
2019-04-21 01:43:22 +0530

    env variables bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+let activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || 'development';
+require('dotenv').config({
+  path: `.env.${activeEnv}`
+});
+
 module.exports = {
   siteMetadata: {
     appbarHeader: 'Droplets of Dream',
